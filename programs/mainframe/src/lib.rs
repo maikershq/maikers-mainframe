f01980cd083ccbc9444d38cdff25cb2dff5ba02d
Soona <code@chainsona.dev>
2025-11-02 18:43:43 +0100

    chore: update source_revision for mainnet deployment

--- programs/mainframe/src/lib.rs
+++ programs/mainframe/src/lib.rs
@@ -15,7 +15,7 @@ security_txt! {
     policy: "https://github.com/maikershq/maikers-mainframe/blob/main/SECURITY.md",
     preferred_languages: "en",
     source_code: "https://github.com/maikershq/maikers-mainframe",
-    source_revision: "8f2d952562f55599f128f0abf984734a57e3e89f",
+    source_revision: "65494192737217aa5228a9ff52d5002081b02447",
     auditors: "To be announced",
     acknowledgements: "Thank you to our security researchers and the Solana community!"
 }
